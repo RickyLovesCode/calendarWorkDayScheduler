9294f418ca4f9fefb5441be9b8f43b6777bff3ee
Ricky27 <Rickyrenteria7@gmail.com>
2022-01-01 11:29:20 -0800

    final touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    $("#currentDay").text(moment().format("MMMM Do, YYYY dddd"));
+
     function colorCode() {
         var thisHour = moment().hours();
         console.log(thisHour);
@@ -34,6 +36,16 @@ $(document).ready(function () {
         console.log("click");
         localStorage.setItem(timeId, note);
     })
+
+    $("#9_oclock .notes").val(localStorage.getItem("9_oclock"));
+    $("#10_oclock .notes").val(localStorage.getItem("10_oclock"));
+    $("#11_oclock .notes").val(localStorage.getItem("11_oclock"));
+    $("#12_oclock .notes").val(localStorage.getItem("12_oclock"));
+    $("#13_oclock .notes").val(localStorage.getItem("13_oclock"));
+    $("#14_oclock .notes").val(localStorage.getItem("14_oclock"));
+    $("#15_oclock .notes").val(localStorage.getItem("15_oclock"));
+    $("#16_oclock .notes").val(localStorage.getItem("16_oclock"));
+    $("#17_oclock .notes").val(localStorage.getItem("17_oclock"));
 
 
 
